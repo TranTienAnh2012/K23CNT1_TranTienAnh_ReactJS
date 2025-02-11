b601b7bc80fbf475be45e31293baaa6008c10744
TranTienAnh2012 <tienanhtran755@gmail.com>
2025-02-11 09:41:39 +0700

    add

---
+++ Day02/hvt-lab02/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React: Trần Tiến Anh 2310900005 20122005 0387742492 K23CNT1
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
